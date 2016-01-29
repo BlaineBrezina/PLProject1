cc4d64f2ece4b5f46c658cd180b815908af6a5f2
Blaine Brezina <blainebrezina@gmail.com>
2016-01-29 15:45:03 -0600

    We rote da code

--- src/Brezina/Customer/Customer.java
+++ src/Brezina/Customer/Customer.java
@@ -4,4 +4,39 @@ package Brezina.Customer;
  * Created by blainebrezina on 1/29/16.
  */
 public class Customer {
+
+    private String name;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private int postalCode;
+
+    public Customer (String name, String address, String city, String state, int postalCode){
+        this.name = name;
+
+        this.address = address;
+
+        this.city = city;
+
+        this.state = state;
+
+        this.postalCode = postalCode;
+    }
+
+    public String getNameAndAddress (){
+        String message =
+                name + "\n" +
+                address + "\n" +
+                city + ", " +
+                state + " " +
+                postalCode;
+
+        return message;
+
+    }
+
 }
--- src/Brezina/Customer/CustomerApp.java
+++ src/Brezina/Customer/CustomerApp.java
@@ -1,10 +1,55 @@
 package Brezina.Customer;
 
+import java.util.Scanner;
+
 public class CustomerApp {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
 
+        Scanner nSA = new Scanner(System.in);
+
+        System.out.println("Welcome to the Customer Viewer\n");
+
+        boolean repeat = true;
+
+        while (repeat) {
+            System.out.println("Enter a customer number: ");
+
+            int customerNumber = nSA.nextInt();
+
+            String info;
+
+            if (customerNumber > 1000 && customerNumber < 1004) {
+
+                Customer hR = CustomerDB.getCustomer(customerNumber);
+                info = hR.getNameAndAddress();
+
+            }
+
+            else {
+                info = "There is no customer number " + customerNumber + " in our records";
+
+            }
+
+            System.out.println(info);
+
+            String response = "";
+
+            while (!(response.equals("y") || response.equals("n"))) {
+
+                System.out.println("Display another customer? (y/n): ");
+                response = nSA.next();
+
+                if (response.equals("n")) {
+                    repeat = false;
+
+                }
+
+            }
+
+        }
+
+        System.out.println("Buh-bye");
+
+    }
 }
